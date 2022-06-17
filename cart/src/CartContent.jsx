1aa781017890768bb65167ab9dadba70de6bb316
oliver <kunhan945@gmail.com>
2022-06-17 16:26:37 +0800

    cart content

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,22 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { jwt } from './cart';
-import Login from './Login';
-import MiniCart from './MiniCart';
+import React, { useState, useEffect } from 'react';
 
-const CartContent = () => {
-  const [token, setToken] = useState('');
+import { cart, clearCart } from 'cart/cart';
+import { currency } from 'home/products';
 
-  useEffect(() => {
-    return jwt.subscribe(v => setToken(v ?? ''));
-  });
+export default function CartContent() {
+  const [items, setItems] = useState([]);
+
+  useEffect(
+    () => cart.subscribe(value => setItems(value?.cartItems ?? [])),
+    []
+  );
 
   return (
-    <div>
-      <div>jwt: {token}</div>
-      <Login />
-      <MiniCart />
-    </div>
+    <>
+      <div className="my-10 grid grid-cols-4 gap-5">
+        {items.map(item => (
+          <React.Fragment key={item.id}>
+            <div>{item.quantity}</div>
+            <img src={item.image} alt={item.name} className="max-h-6" />
+            <div>{item.name}</div>
+            <div className="text-right">
+              {currency.format(item.quantity * item.price)}
+            </div>
+          </React.Fragment>
+        ))}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="text-right" id="grand_total">
+          {currency.format(items.reduce((a, v) => a + v.quantity * v.price, 0))}
+        </div>
+      </div>
+      {items.length > 0 && (
+        <div className="flex mb-10">
+          <div className="flex-grow">
+            <button
+              id="clearcart"
+              className="bg-white border border-blue-500 text-blue-800 py-2 px-5 text-sm"
+              onClick={clearCart}
+            >
+              Clear Cart
+            </button>
+          </div>
+          <div className="flex-end">
+            <button
+              className="bg-blue-500 text-white py-2 px-5 text-sm"
+              onClick={clearCart}
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
-};
-
-export default CartContent;
+}
