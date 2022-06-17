1f2ce9ce9052c3832bcdd73ce2feab53413da6c0
oliver <kunhan945@gmail.com>
2022-06-17 14:39:39 +0800

    pdp with shared function

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// react-router-dom v6
+
 import Footer from 'home/Footer';
 import Header from 'home/Header';
 
 import './index.scss';
+import PDPContent from './PDPContent';
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP Page Content</div>
-    <Footer />
-  </div>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
